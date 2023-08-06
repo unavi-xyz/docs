bbfc8fe4d54682560553d802b6889a30ebdb2024
Kayh <basedlainpilled@gmail.com>
2023-08-06 11:33:16 -0400

    setup unavi info

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,28 +3,27 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'UNAVI Docs',
+      logo: {
+        light: './src/assets/Logo-Dark.png',
+        dark: './src/assets/Logo-Light.png',
+      },
+      social: {
+        discord: 'https://discord.com/invite/VCsAEneUMn',
+        github: 'https://github.com/unavi-xyz/unavi',
+        twitter: 'https://twitter.com/unavi_xyz',
+      },
+      sidebar: [
+        {
+          label: 'Overview',
+          autogenerate: { directory: 'overview' },
+        },
+      ],
+    }),
+  ],
 
-	// Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-	image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+  image: { service: { entrypoint: 'astro/assets/services/sharp' } },
 });
