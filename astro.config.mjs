84b6f21cddaf9412a1e15d8310ff9d5731221e49
Kayh <basedlainpilled@gmail.com>
2024-01-11 13:34:23 -0500

    update metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
   integrations: [
     starlight({
       title: "UNAVI Docs",
-      description: "An open-source VR social platform built on The Wired.",
+      description: "An open-source VR social platform.",
       editLink: {
         baseUrl: "https://github.com/unavi-xyz/docs/edit/main/",
       },
